62e438e79e8307a49fc2492597386bb71708279e
sajicode <sajioloye@gmail.com>
2020-02-03 23:30:41 +0100

    setup app navigation

--- App.js
+++ App.js
@@ -1,8 +1,28 @@
 import React from 'react';
+import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
 import { StyleSheet, Text, View } from 'react-native';
 import AccountScreen from './src/screens/AccountScreen';
 import SignupScreen from './src/screens/SignupScreen';
 import SigninScreen from './src/screens/SigninScreen';
-import TrackCreate from './src/screens/TrackCreateScreen';
+import TrackCreateScreen from './src/screens/TrackCreateScreen';
 import TrackDetailScreen from './src/screens/TrackDetailScreen';
 import TrackListScreen from './src/screens/TrackListScreen';
+
+const switchNavigator = createSwitchNavigator({
+	loginFlow: createStackNavigator({
+		Signup: SignupScreen,
+		Signin: SigninScreen
+	}),
+	mainFlow: createBottomTabNavigator({
+		trackListFlow: createStackNavigator({
+			TrackList: TrackListScreen,
+			TrackDetail: TrackDetailScreen
+		}),
+		TrackCreate: TrackCreateScreen,
+		Account: AccountScreen
+	})
+});
+
+export default createAppContainer(switchNavigator);
