567bfa36e9b924333e31d6b12298ac1fbd76a896
sajicode <sajioloye@gmail.com>
2020-02-03 15:30:56 +0100

    setup app boilerplate

---
+++ App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import AccountScreen from './src/screens/AccountScreen';
+import SignupScreen from './src/screens/SignupScreen';
+import SigninScreen from './src/screens/SigninScreen';
+import TrackCreate from './src/screens/TrackCreateScreen';
+import TrackDetailScreen from './src/screens/TrackDetailScreen';
+import TrackListScreen from './src/screens/TrackListScreen';
