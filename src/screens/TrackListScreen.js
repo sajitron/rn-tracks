6a98710df9acefa625a4e56cee5297d73e3dd957
sajicode <sajioloye@gmail.com>
2020-02-18 00:19:06 +0100

    show track list & details

--- src/screens/TrackListScreen.js
+++ src/screens/TrackListScreen.js
@@ -1,11 +1,27 @@
-import React from 'react';
-import { View, StyleSheet, Text, Button } from 'react-native';
+import React, { useContext } from 'react';
+import { View, StyleSheet, Text, FlatList, TouchableOpacity } from 'react-native';
+import { NavigationEvents } from 'react-navigation';
+import { ListItem } from 'react-native-elements';
+import { Context as TrackContext } from '../context/TrackContext';
 
+// * navigation events helps us to note when we have navigated to a screen
 const TrackListScreen = ({ navigation }) => {
+	const { state, fetchTracks } = useContext(TrackContext);
 	return (
 		<View>
+			<NavigationEvents onWillFocus={fetchTracks} />
 			<Text>Track List Screen</Text>
-			<Button title="Go to Track Detail" onPress={() => navigation.navigate('TrackDetail')} />
+			<FlatList
+				data={state}
+				keyExtractor={(item) => item._id}
+				renderItem={({ item }) => {
+					return (
+						<TouchableOpacity onPress={() => navigation.navigate('TrackDetail', { _id: item._id })}>
+							<ListItem chevron title={item.name} />
+						</TouchableOpacity>
+					);
+				}}
+			/>
 		</View>
 	);
 };
