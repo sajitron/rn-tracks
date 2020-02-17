432dfff3a6ec2b55486a2b6cbffd67c194425721
sajicode <sajioloye@gmail.com>
2020-02-17 23:46:48 +0100

    setup automatic authentication

--- src/api/tracker.js
+++ src/api/tracker.js
@@ -1,5 +1,24 @@
 import axios from 'axios';
+import { AsyncStorage } from 'react-native';
 
-export default axios.create({
+const instance = axios.create({
 	baseURL: 'https://e4b3e9d9.ngrok.io'
 });
+
+instance.interceptors.request.use(
+	//* called before request
+	(config) => {
+		const token = AsyncStorage.getItem('token');
+		if (token) {
+			config.headers.Authorization = `Bearer ${token}`;
+		}
+
+		return config;
+	},
+	//* called if there is an error with request
+	(err) => {
+		return Promise.reject(err);
+	}
+);
+
+export default instance;
